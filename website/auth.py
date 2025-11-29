82546003caa4a6b2a188068dbeec9c75ba3e3c49
J-Adithye <adithyej8@gmail.com>
2025-11-29 21:56:50 +0530

    used flask login manager to . home only visible when logged in .logout redirect to login page etc

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint,render_template,request,flash,redirect,url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-from flask import login_user,login_required,logout_user,current_user
+from flask_login import login_user,login_required,logout_user,current_user
 
 auth = Blueprint('auth',__name__)
 
@@ -16,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password,password):
                 flash('logged in', category='success')
+                login_user(user,remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('incorrect password',category='error')
@@ -44,7 +45,9 @@ def signup():
             new_user = User(email = email, first_name=first_name, last_name = last_name,password = generate_password_hash(password))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user,remember=True)
             flash("Account Created", category='success')
+
             return redirect(url_for('views.home'))
 
     return render_template("signup.html")
@@ -53,5 +56,7 @@ def signup():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('auth.login'))
